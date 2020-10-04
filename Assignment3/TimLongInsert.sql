4215385a6426be583e835d979636e9278da21f0b
TimRLong <66142120+TimRLong@users.noreply.github.com>
2020-10-04 08:16:48 -0400

    Add files via upload

---
+++ Assignment3/TimLongInsert.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (first_name, last_name, email_address, password) VALUES
+('Tim', 'Long', 'S00913775@acad.tri-c.edu', '456789');
+
+SELECT * FROM timlong_my_guitar_shop.customers;
